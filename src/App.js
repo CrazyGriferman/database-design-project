9f4a024223fbe529214a4c3ace83099d730cc99e
CrazyGriferman <251031557@qq.com>
2021-04-14 21:28:55 +0800

    使用react-router, 登陆页面初始化

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import MainPage from '../src/components/common/MainPage';
+import Login from '../src/components/common/Login';
+
 
 function App() {
+
   return (
-    <>
-      <MainPage />
-    </>
+
+      <Router>
+          
+        <Switch>
+          <Route path="/main">
+            <MainPage />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+
+      </Router>
+
   );
 }
 
